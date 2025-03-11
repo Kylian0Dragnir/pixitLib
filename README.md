cf021e4a1745605714e9c639062bfa3509896093
Kiyut0 <kylianmermin1@gmail.com>
2025-03-12 00:58:50 +0100

    pixitlib 0.1
    add functions :
    __init__
    connect
    send_frame
    listen_for_commands
    close

---
+++ README.md
@@ -0,0 +1,2 @@
+#Installer :
+pip installijfgr
--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="ma_lib",  # Nom de la librairie
-    version="0.1",  # Version initiale
-    packages=find_packages(),  # Recherche automatiquement les packages dans le dossier
-    install_requires=[],  # Dépendances à ajouter si besoin
+    name="pixitlib",  
+    version="0.1",  
+    packages=find_packages(), 
+    install_requires=[],  
     author="Mermin Kylian",
     author_email="kylianmermin1@gmail.com",
     description="Une librairie pour se connecter à la console du jeu.",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/Kylian0Dragnir/jeuPixel",  # Remplace par ton URL GitHub
+    url="https://github.com/Kylian0Dragnir/pixitLib", 
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",  # Version minimale de Python requise
+    python_requires=">=3.6",  
 )
